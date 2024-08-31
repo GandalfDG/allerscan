c7d2ed3674d055707d94c51d4a4ee5f5687ef8e8
Jack Case <jackacase@gmail.com>
2024-08-31 12:29:26 +0000

    wired up template for scanner app

--- allerscan-web/scanner/views.py
+++ allerscan-web/scanner/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import loader
 
-def index(request):
-    return HttpResponse("Hello, world. Welcome to the scanner app.")
+def scanpage(request):
+    template = loader.get_template("scanner/index.html")
+    return HttpResponse(template.render(None, request))
