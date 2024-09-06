2556acb1a7f03d055cca1864e4f9b5690aa3deca
Jack Case <jackacase@gmail.com>
2024-09-06 18:30:04 +0000

    readapi.py can successfully make a request to my endpoint

---
+++ allerscan-web/scanner/readapi.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+import os
+import json
+import requests
+
+load_dotenv()
+
+api_endpoint = os.getenv("READ_API_ENDPOINT")
+api_key = os.getenv("READ_API_KEY")
+
+def read_api_request(image_url):
+    query_params = {
+        "api-version": "2023-04-01-preview",
+        "features": "read"
+    }
+
+    request_body = json.dumps({
+        "url": image_url
+    })
+
+    request_header = {
+        "Ocp-Apim-Subscription-Key": api_key,
+        "Content-Type": "application/json"
+    }
+    response = requests.post(api_endpoint + "computervision/imageanalysis:analyze", params=query_params, headers=request_header, data=request_body)
+
+    return response
