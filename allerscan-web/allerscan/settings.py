c116a9afac3ffc84f9916ff37b721a98baba7847
Jack Case <jackacase@gmail.com>
2024-10-09 14:17:54 +0000

    add some meta info and set up static directory for favicon

--- allerscan-web/allerscan/settings.py
+++ allerscan-web/allerscan/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 if os.getenv("DJANGO_MEDIA_ROOT"):
     MEDIA_ROOT = os.getenv("DJANGO_MEDIA_ROOT")
